453095a2156cf1d72047d7b77eb97e355d90fe74
Anton Minakov <int-64@ya.ru>
2020-11-15 15:30:48 +0500

    task 1

---
+++ lesson1/task01.py
@@ -0,0 +1,12 @@
+intValue = 123
+floatValue = 123.456
+strValue = "text"
+boolValue = True
+
+print(f"int value:{intValue}")
+print(f"float value:{floatValue}")
+print(f"str value:{strValue}")
+print(f"bool value:{boolValue}")
+
+number = int(input("Введите число >>> "))
+print(number)
